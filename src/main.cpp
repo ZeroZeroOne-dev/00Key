38046be1f6abaaa557f7a122f6f828f5ae802f12
ZeroZeroOne <jeffrey.smets@gmail.com>
2020-08-24 16:16:28 +0200

    basic keyboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,52 @@
 #include <Arduino.h>
 
+const byte rowCount = 2;
+const byte colCount = 2;
+const byte rowPins[] = {23, 22};
+const byte colPins[] = {34, 33};
+
+void printKey(int row, int col)
+{
+  Serial.print("row: ");
+  Serial.print(row + 1);
+  Serial.print(" col: ");
+  Serial.print(col + 1);
+  Serial.println();
+}
+
 void setup()
 {
+  Serial.begin(115200);
+
+  for (byte i = 0; i < colCount; i++)
+  {
+    pinMode(colPins[i], INPUT_PULLUP);
+  }
+
+  for (byte i = 0; i < rowCount; i++)
+  {
+    pinMode(rowPins[i], OUTPUT);
+  }
 }
 
 void loop()
 {
-  Keyboard.print("Hello World ");
+  for (byte i = 0; i < rowCount; i++)
+  {
+    byte row = rowPins[i];
+
+    digitalWrite(row, LOW);
+
+    for (byte z = 0; z < colCount; z++)
+    {
+      byte col = colPins[z];
+
+      if (digitalRead(col) == LOW)
+      {
+        printKey(i, z);
+      }
+    }
+
+    digitalWrite(row, HIGH);
+  }
 }
