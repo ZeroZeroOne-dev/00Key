8c9423b5fc972aa58b76e216ba6678d584e6104d
ZeroZeroOne <jeffrey.smets@gmail.com>
2020-10-30 00:18:48 +0100

    KeyMap

--- firmware/include/Globals.h
+++ firmware/include/Globals.h
@@ -1,13 +1,13 @@
 #ifndef Globals_h
 #define Globals_h
 
-#define COL_COUNT 2
-#define ROW_COUNT 2
+#define COL_COUNT 16
+#define ROW_COUNT 6
 #define SCAN_DELAY 5
 #define PIN_CHANGE_DELAY 500
 
+#define KEY_NOKEY 0
 #define KEY_00KEY_MOVELAYER 1000
-
 #define KEY_00KEY_BLUP 1001
 #define KEY_00KEY_BLDOWN 1002
 
